59e060b0146f65d00f005970019e60d37c4ff823
de4life <ageev_artur@ukr.net>
2021-07-22 22:08:04 +0300

    Initial Commit

---
+++ NoteTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteTask
+//
+//  Created by Артур on 22.07.2021.
+//  Copyright © 2021 Артур. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
